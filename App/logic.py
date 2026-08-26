7ad599f691d005e6486a22779d077035ab264b69
JimmyMD20 <j.mujicad@uniandes.edu.co>
2026-08-26 15:08:40 -0500

    Agregar a logic y crear "stack.py"

--- App/logic.py
+++ App/logic.py
@@ -29,6 +29,7 @@ import os
 import time
 from DataStructures.List import array_list as lt
 # TODO Importar las librerías correspondientes para el manejo de pilas y colas
+from DataStructures.Queue import queue as q
 
 data_dir = os.path.dirname(os.path.realpath('__file__')) + '/Data/'
 
